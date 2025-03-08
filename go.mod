0f239004e4e5d65a860e8974b50e5a2facc080a6
takeshiTran <thuyvanthai2013@gmail.com>
2025-03-08 23:07:42 +0700

    update grpc

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.25.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.2 // indirect
@@ -38,6 +39,8 @@ require (
 	golang.org/x/sync v0.11.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
+	google.golang.org/grpc v1.71.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/postgres v1.5.11 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,67 @@
 package main
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
+	service "golang-minazuki/protobuf"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
 )
+
+type CategoryServiceServer struct {
+	service.UnimplementedCategoryServiceServer
+}
+
+func (s *CategoryServiceServer) GetCategory(ctx context.Context, request *service.CategoryRequest) (*service.CategoryResponse, error) {
+	log.Printf("Category Request Received: %v", request)
+	return &service.CategoryResponse{
+		Id:     request.GetId(),
+		Name:   "thaitx",
+		Detail: "test detail",
+	}, nil
+}
+
+func (s *CategoryServiceServer) mustEmbedUnimplementedCategoryServiceServer() {
+	//TODO implement me
+	panic("implement me")
+}
+
+func initGrpcServer() {
+	log.Printf("GRPC server INIT >>>")
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	srv := grpc.NewServer()
+
+	service.RegisterCategoryServiceServer(srv, &CategoryServiceServer{})
+	log.Printf("gRPC server listening at %v", lis.Addr())
+	err = srv.Serve(lis)
+	if err != nil {
+		log.Fatalf("failed to serve gRPC: %v", err)
+	}
+}
+
+func initGrpcClient() (connection *grpc.ClientConn) {
+	log.Printf("GRPC client INIT >>>")
+	conn, err := grpc.NewClient("localhost:50052", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to create gRPC client: %v", err)
+	}
+	port := conn.GetState().String()
+	log.Printf("gRPC client listening at %v", port)
+	return conn
+}
 
 type Category struct {
 	gorm.Model
@@ -21,6 +76,7 @@ func (Category) TableName() string {
 var db *gorm.DB
 
 func connectDatabase() *gorm.DB {
+	log.Printf("Database server connecting >>>")
 	dsn := "host=host.docker.internal user=postgres password=2716 dbname=postgres port=5432 sslmode=disable search_path=local"
 	var err error
 
@@ -54,16 +110,59 @@ func getAllCategory(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, allCategory)
 }
 
-func main() {
+func getCategory(c *gin.Context) {
+	var allCategory []Category
+	if err := db.Find(&allCategory).Error; err != nil {
+		log.Printf("Failed to get all category: %v", err)
+		return
+	}
+	c.IndentedJSON(http.StatusOK, allCategory)
+}
 
-	//set up database connection
-	connectDatabase()
+func getCategoryById(c *gin.Context, connection *grpc.ClientConn) {
+	log.Printf("Getting category by id: %v", c.Param("id"))
+	id, err := strconv.Atoi(c.DefaultQuery("id", "0"))
+	if err != nil {
+		c.JSON(400, gin.H{"message": "Bad Request"})
+	}
+	conn := service.NewCategoryServiceClient(connection)
+	response, err := conn.GetCategory(c, &service.CategoryRequest{
+		Id: int32(id),
+	})
+	c.IndentedJSON(http.StatusOK, response)
+}
 
-	//set up application
+func initGinServer() {
+	log.Printf("GIN server INIT >>>")
 	rout := gin.Default()
+	port := ":3004"
 	rout.GET("/minazuki", func(ctx *gin.Context) {
 		getAllCategory(ctx)
 	})
+	rout.GET("/minazuki/getCategoryByID", func(ctx *gin.Context) {
+		getCategoryById(ctx, initGrpcClient())
+	})
 	rout.POST("/minazuki", createCategory)
-	rout.Run(":3004")
+	err := rout.Run(port)
+	if err != nil {
+		log.Fatalf("Failed to start GIN server: %v", err)
+	}
+	log.Printf("GIN server listening at %v", port)
+}
+
+func main() {
+
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM)
+
+	//set up database connection
+	go connectDatabase()
+
+	//set up grpc server
+	go initGrpcServer()
+
+	//set up application
+	go initGinServer()
+
+	<-stopChan
 }
